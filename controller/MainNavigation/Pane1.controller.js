ec3d95fda580e4f1b18d5b700485e5fd467fd872
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-06-30 11:14:56 +0200

    init

--- controller/MainNavigation/Pane1.controller.js
+++ controller/MainNavigation/Pane1.controller.js
@@ -1,9 +1,16 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function (Controller) {
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+], function (Controller, JSONModel) {
     "use strict";
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.Pane1");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.Pane1", {
+        onInit: function () {
+            var oModel = new JSONModel(sap.ui.require.toUrl("DemoUXapp/content") + "/Tree.json");
+            this.getView().setModel(oModel);
+            //this.byId("Tree").expandToLevel(1);
+        }
+    });
 
     return Controller;
 });
